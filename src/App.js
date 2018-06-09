69e5a0b8cd4c622b3863eaa645c980b458c8a564
Kenton Wan <kenton.wan@gmail.com>
2018-06-09 15:17:45 -0700

    Assignment 3: Completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route,Link } from 'react-router-dom';
 import './App.css';
 import Library from './components/Library';
 import Landing from './components/Landing';
+import Album from './components/Album';
 
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
         <main>
           <Route exact path="/" component={ Landing } />
           <Route path="/library" component={ Library } />
+          <Route path="/album" component={ Album } />
         </main>
       </div>
     );
---
+++ src/components/Album.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Album = () => (
+  <section className="album">
+    Album page will go Here
+  </section>
+);
+
+export default Album;
