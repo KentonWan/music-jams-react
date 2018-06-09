54cf0163ebb8515a1015e88eabe4b16e8e81558f
Kenton Wan <kenton.wan@gmail.com>
2018-06-09 15:08:06 -0700

    Checkpoint 3: Completed

---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Landing = () => (
+  <section className="landing">
+    Landing page will go Here
+  </section>
+);
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Library = () => (
+  <section className="library">
+    Library will go Here
+  </section>
+);
+
+export default Library;
