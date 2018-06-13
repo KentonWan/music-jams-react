682b9891452667014f26a1f084c483b7b61c2744
Kenton Wan <kenton.wan@gmail.com>
2018-06-13 11:00:01 -0700

    Checkpoint 8: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props){
@@ -45,10 +46,13 @@ class Album extends Component {
     }
   }
 
-  //handleMouseEnter() {
-  //  this.setState({imgSrc: <ion-icon name="play"></ion-icon>})
-  //  ;
-  //}
+  handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0,currentIndex-1);
+    const newSong= this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
 
 
   render() {
@@ -94,6 +98,10 @@ class Album extends Component {
           }
           </tbody>
         </table>
+        <PlayerBar isPlaying={this.state.isPlaying}
+                  currentSong={this.state.currentSong}
+                  handleSongClick={()=>this.handleSongClick(this.state.currentSong)}
+                  handlePrevClick={()=>this.handlePrevClick()}/>
       </section>
     );
   }
